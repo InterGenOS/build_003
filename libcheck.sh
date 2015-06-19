655372eebdefb33ef71d3836c00d33c408edeb87
InterGenOS <chris@intergenstudios.com>
2015-06-18 23:00:48 -0500

    Expanded definition for libcheck.sh

--- libcheck.sh
+++ libcheck.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Checks for the triad of libgmp.la, libmpfr.la, and libmpc.la
 # Either all three should be present or absent, but not only one or two
+# If the problem exists on your system, either rename or delete the .la files or
+# install the appropriate missing package.
 
 for lib in lib{gmp,mpfr,mpc}.la; do
   echo $lib: $(if find /usr/lib* -name $lib|
