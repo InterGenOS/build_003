4a82119ad436512895ad4012659bffc44035fd3d
InterGenOS <chris@intergenstudios.com>
2015-06-27 08:58:09 -0500

    re-working 'setup' - IN DEVELOPMENT

--- build_system.sh
+++ build_system.sh
@@ -1336,7 +1336,7 @@ BUILD_E2FSPROGS () {
     chmod -v u+w /usr/lib/{libcom_err,libe2p,libext2fs,libss}.a &&
     gunzip -v /usr/share/info/libext2fs.info.gz
     install-info --dir-file=/usr/share/info/dir /usr/share/info/libext2fs.info &&
-    cd /sources/..
+    cd /sources
     rm -rf e2fsprogs-1.42.12
     printf "\n\n"
     sleep 3
